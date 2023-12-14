930e7ed77ddcd2eb74f5052e4f56c92fe233a72f
Himal <himalgole1122@gmail.com>
2023-12-14 22:06:18 +0545

    animation1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,20 @@
+var h = window.innerHeight;
+var w = window.innerWidth;
+
 function pageload(){
     setTimeout(() => {
         document.querySelector(".preloader").style.display = "none";
         document.body.style.overflowY = "scroll";
-    }, 700);
+        document.querySelector(".header").classList.add("header-animated");
+        document.querySelector(".home .p1").classList.add("p1-animate");
+        document.querySelector(".home h2").classList.add("h2-animate");
+        document.querySelector(".home h3").classList.add("h3-animate");
+
+        var list = document.querySelectorAll(".bio p");
+        list.forEach(item =>{
+            item.classList.add("p-animate")
+        })
+    }, 200);
    
 }
 function show_mbl_nav()
